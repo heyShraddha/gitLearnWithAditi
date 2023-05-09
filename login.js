88b28f10ddf396e26669f6fa7eefe81c43d0fb85
i524909 <shraddha.gulati@sap.com>
2023-05-09 20:26:12 +0530

    Added Login Logout function

---
+++ login.js
@@ -0,0 +1,7 @@
+const user = "Aditi";
+function login(){
+    if(user === "Aditi"){
+        console.log("User is Authorized");
+    }
+}
+login();
---
+++ logout.js
@@ -0,0 +1,7 @@
+const user = "Aditi";
+function logout(){
+    if(user === "Aditi"){
+        console.log("User is successfully logged out");
+    }
+}
+logout();
