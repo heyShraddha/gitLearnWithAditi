66b74cea1212007f9018f235250b682489d2256c
i524909 <shraddha.gulati@sap.com>
2023-05-09 20:38:58 +0530

    Login With Google

--- login.js
+++ login.js
@@ -1,7 +1,15 @@
 const user = "Aditi";
+const userEmail = "aditi@gmail.com";
 function login(){
     if(user === "Aditi"){
         console.log("User is Authorized");
     }
 }
 login();
+
+function loginWithGoogle(){
+    if(userEmail === "aditi@gmail.com"){
+        console.log("User is Authorized");
+    }
+}
+loginWithGoogle();
